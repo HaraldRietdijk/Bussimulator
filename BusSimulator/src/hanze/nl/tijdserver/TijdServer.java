f3ae9f4c5989459d79f76125818e9bc0f23e1c9c
HaraldRietdijk <h.h.rietdijk@pl.hanze.nl>
2019-02-21 13:27:36 +0100

    TijdServer

---
+++ BusSimulator/src/hanze/nl/tijdserver/TijdServer.java
@@ -0,0 +1,31 @@
+package hanze.nl.tijdserver;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class TijdServer {
+
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(5002), 0);
+        server.createContext("/TimeServer", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "This is the response";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+
+}
